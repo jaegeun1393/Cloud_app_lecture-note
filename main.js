436b75a02f12a65788a94a74c503a376324f22d5
jaegeun1393 <46430624+jaegeun1393@users.noreply.github.com>
2021-04-07 15:05:23 -0700

    Update main.js

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ function asciiART(err, data) {
 
 figlet("Callback", asciiART);
 
-const circum = require('./circle'); //bring another js file to the main js
+//
+const circle = require('./circle'); //bring another js file to the main js
 //require will bring the result
 
 console.log("== circum with radius(5: ", circle.circum(5)); //circle is object and circum is function inside the object
+console.log("== circum with radius(5: ", circle.area(5)); //call area
